45952528b9e9857d927c9ca79e42a935c2f23d52
fffan64 <ff.fan@hotmail.fr>
2018-05-16 22:38:07 +0900

    Inventory System v1

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class Item {
+
+    public string itemName;
+    public int itemID;
+    public string itemDesc;
+    public Texture2D itemIcon;
+    public int itemPower;
+    public int itemSpeed;
+    public ItemType itemType;
+
+    public enum ItemType
+    {
+        Weapon,
+        Consumable,
+        Quest
+    }
+
+    public Sprite[] allSpritesIcons;
+
+    Texture2D ConvertSpriteToTexture(Sprite sprite)
+    {
+        try
+        {
+            //Debug.Log("sprite.rect.width != sprite.texture.width / " + sprite.rect.width + " : " + sprite.texture.width);
+            if (sprite.rect.width != sprite.texture.width)
+            {
+                Texture2D newText = new Texture2D(Mathf.CeilToInt(sprite.textureRect.width), Mathf.CeilToInt(sprite.textureRect.height));
+                //Color[] colors = newText.GetPixels();
+                Color[] newColors = sprite.texture.GetPixels(Mathf.CeilToInt(sprite.textureRect.x),
+                                                             Mathf.CeilToInt(sprite.textureRect.y),
+                                                             Mathf.CeilToInt(sprite.textureRect.width),
+                                                             Mathf.CeilToInt(sprite.textureRect.height));
+                //Debug.Log(colors.Length + "_" + newColors.Length);
+                newText.SetPixels(newColors);
+                newText.filterMode = FilterMode.Point;
+                newText.Apply();
+                return newText;
+            }
+            else
+                return sprite.texture;
+        }
+        catch(System.Exception e)
+        {
+            Debug.LogError(e);
+            return sprite.texture;
+        }
+    }
+
+    public Item(string name, int id, string desc, int power, int speed, ItemType type)
+    {
+        if (allSpritesIcons == null)
+        {
+            allSpritesIcons = Resources.LoadAll<Sprite>("Item Icons/");
+        }
+
+        itemName = name;
+        itemID = id;
+        itemDesc = desc;
+        Sprite s = allSpritesIcons.Where(x => x.name == name).SingleOrDefault();
+        if(s)
+        {
+            itemIcon = ConvertSpriteToTexture(s);
+        }
+        itemPower = power;
+        itemSpeed = speed;
+        itemType = type;
+    }
+
+    public Item()
+    {
+        itemID = -1;
+    }
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+    public List<Item> items = new List<Item>();
+
+    private void Start()
+    {
+        items.Add(new Item("Amulet of Prayers", 0, "An amulet enchanted by prayers", 2, 0, Item.ItemType.Weapon));
+        items.Add(new Item("White Shirt", 1, "A shirt that is white", 0, 0, Item.ItemType.Weapon));
+        items.Add(new Item("Power Potion", 2, "A potion that temporary increase your power", 0, 0, Item.ItemType.Consumable));
+    }
+}
